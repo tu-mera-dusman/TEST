bc08918aadefeca757a49517a99a8f25bea7a7ba
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2025-02-04 23:55:14 +0530

    Update git.py

--- PROMUSIC/core/git.py
+++ PROMUSIC/core/git.py
@@ -39,38 +39,33 @@ def git():
         UPSTREAM_REPO = config.UPSTREAM_REPO
     try:
         repo = Repo()
-        LOGGER(__name__).info("Git Client Found [VPS DEPLOYER]")
+        LOGGER(__name__).info(f"Git Client Found [VPS DEPLOYER]")
     except GitCommandError:
-        LOGGER(__name__).info("Invalid Git Command")
+        LOGGER(__name__).info(f"Invalid Git Command")
     except InvalidGitRepositoryError:
         repo = Repo.init()
         if "origin" in repo.remotes:
             origin = repo.remote("origin")
         else:
             origin = repo.create_remote("origin", UPSTREAM_REPO)
-        
         origin.fetch()
-        
-        # Check if the branch exists
-        upstream_branch_ref = origin.refs.get(config.UPSTREAM_BRANCH, "main")
-        if upstream_branch_ref:
-            repo.create_head(config.UPSTREAM_BRANCH, upstream_branch_ref)
-            repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(upstream_branch_ref)
-            repo.heads[config.UPSTREAM_BRANCH].checkout(True)
-        else:
-            LOGGER(__name__).error(f"Branch '{config.UPSTREAM_BRANCH}' not found in remote repository.")
-
+        repo.create_head(
+            config.UPSTREAM_BRANCH,
+            origin.refs[config.UPSTREAM_BRANCH],
+        )
+        repo.heads[config.UPSTREAM_BRANCH].set_tracking_branch(
+            origin.refs[config.UPSTREAM_BRANCH]
+        )
+        repo.heads[config.UPSTREAM_BRANCH].checkout(True)
         try:
             repo.create_remote("origin", config.UPSTREAM_REPO)
         except BaseException:
             pass
-        
         nrs = repo.remote("origin")
         nrs.fetch(config.UPSTREAM_BRANCH)
         try:
             nrs.pull(config.UPSTREAM_BRANCH)
         except GitCommandError:
             repo.git.reset("--hard", "FETCH_HEAD")
-        
         install_req("pip3 install --no-cache-dir -r requirements.txt")
-        LOGGER(__name__).info("Fetching updates from upstream repository...")
+        LOGGER(__name__).info(f"Fetching updates from upstream repository...")
