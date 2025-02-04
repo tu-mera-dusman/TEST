2afaf5e3235240af025c5c99ad60fcfc1e6f6c5a
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2025-02-04 23:37:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -54,7 +54,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TeamProBots/ZeixBots",
+    "https://github.com/vishalpandeynkp1/vishal",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
