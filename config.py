185464e29a9477d479eae5b1a149546dc3a005c4
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2025-02-05 11:36:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -68,10 +68,10 @@ GIT_TOKEN = getenv(
 
 # ------------------------------------------------------------------------
 # -------------------------------------------------------------------------
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/nobita_all_bot")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/nobita_all_bot")
-SOURCE = getenv("SOURCE", "https://t.me/nobita_all_bot")
-CHAT = getenv("CHAT", "https://t.me/nobita_all_bot")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/sundarma01")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/Chatni_girl")
+SOURCE = getenv("SOURCE", "")
+CHAT = getenv("CHAT", "https://t.me/+BXxvwWdPfXsyYTA1")
 # ------------------------------------------------------------------------------
 # -------------------------------------------------------------------------------
 
